a35ad83ab0f1703e7ed547e133d36a688d21a6f1
JoelNash-Xuuki <joelnash@xuuki.xyz>
2023-01-24 03:28:50 +0000

    added input.txt and recscn

--- src/101.orc
+++
@@ -1,9 +0,0 @@
-sr 		= 		44100
-kr 		= 		4410
-ksmps 	= 		10
-nchnls 	= 		1
-
-		instr  	101
-a1   	oscil   10000, p4, 1
-       	out     a1
-		endin
--- src/mmw.sh
+++ src/mmw.sh
@@ -17,7 +17,7 @@ renderMix(){
 }
 
 playMix(){
-  csound -odac $2
+  csound -odac $1
 }
 
 audioToMP4(){
@@ -56,12 +56,28 @@ sequenceImages(){
 
 animate(){
 
-  ffmpeg -i ~/motion-picture/sequence.mp4 -filter:v "setpts=5.712*PTS,minterpolate='fps=24:mb_size=16:search_param=1000:vsbmc=0:scd=none:mc_mode=obmc:me_mode=bidir:me=ds'" -r 120 ~/motion-picture/sequence-result.mp4
+  ffmpeg -i ~/motion-picture/sequence.mp4 -filter:v "setpts=0.714*PTS,minterpolate='fps=8:mb_size=16:search_param=1000:vsbmc=0:scd=none:mc_mode=obmc:me_mode=bidir:me=ds'" -r 420 ~/motion-picture/sequence-result.mp4
 
 }
 
+overlayWatermarktt(){
+  ffmpeg -i $1 -vf "movie=$2[watermark]; [watermark]scale=240x135 [watermark2];[in][watermark2] overlay=(W/0.7)-(overlay_w/1):(H/0.9)-(overlay_h/1):enable='between(t,($4),($5))'[out]" $3
+  mv $3 $1
+}
+
 overlayWatermark(){
-  ffmpeg -i $1 -vf "movie=$2 [watermark]; [watermark]scale=100x100 [watermark2];[in][watermark2] overlay=(W/1)-(overlay_w/1):(H/1)-(overlay_h/1):enable='between(t,($4),($5))'[out]" $3
+  ffmpeg -i $1 -vf "movie=$2[watermark]; [watermark]scale=240x135 [watermark2];[in][watermark2] overlay=(W/0.85)-(overlay_w/1):(H/0.85)-(overlay_h/1):enable='between(t,($4),($5))'[out]" $3
+  mv $3 $1
+}
+
+
+overlayImage(){
+  ffmpeg -i $1 -vf "movie=$2 [layer]; [layer]scale=380:380[layer2];[in][layer2] overlay=240:80 [out]" $3
+  mv $3 $1
+}
+
+overlayImagett(){
+  ffmpeg -i $1 -vf "movie=$2 [layer]; [layer]scale=380:380[layer2];[in][layer2] overlay=40:240 [out]" $3
   mv $3 $1
 }
 
@@ -77,6 +93,26 @@ createIGImages(){
    y=$2
    convert -size 1080x1080 plasma:lightblue -swirl -180 $3 -swirl 50 -composite "$4" -crop 0x0+0+0 -scale 270x270 -gravity east -geometry +0+60 -composite $5
 }
+
+#crop(){
+  #magick /home/images/test.jpg -resize 1080x1080\! /home/images/scale.jpg
+  #magick /home/images/scale.jpg -crop 1080x1080+0+0 /home/images/crop.jpg
+#}
+
+addTextToVid(){
+  ffmpeg -i $1 -vf "drawtext=fontcolor=pink:fontsize=13.3:fontfile=/home/joel/.fonts/xuukitype1.ttf:x=W-(W/1.4):y=H-(H/1.2):enable='between(t,0,30):textfile=/home/joel/projects_/03102022180354/src/text.txt'" $2
+  mv $2 $1
+}
+
+addTextToVidtt(){
+  ffmpeg -i $1 -vf "drawtext=fontcolor=pink:fontsize=13.3:fontfile=/home/joel/.fonts/xuukitype1.ttf:x=W-(W/1.1):y=H-(H/1.4):enable='between(t,0,30):textfile=/home/joel/projects_/03102022180354/src/text.txt'" $2
+  mv $2 $1
+}
+
+play(){
+  ffplay $1
+}
+
 
 "$@"
 
