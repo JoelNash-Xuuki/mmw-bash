f0480f9b917ba2def2b38a509e78da45ba670ae1
JoelNash-Xuuki <joelnash@xuuki.xyz>
2023-03-18 23:28:04 +0000

    added subtitles using midi time stamp

--- bin/ip.sh
+++ bin/ip.sh
@@ -93,6 +93,4 @@ runMethod(){
 #ffmpeg -ss 00:00:00 -i /home/joel/motion-picture/03102022180354.mp4 -t 22.784 -filter_complex "[0:a]showspectrum=s=854x480:mode=combined:slide=scroll:saturation=0.2:scale=log,format=yuv420p[v]" -map "[v]" -map 0:a -b:v 700k -b:a 360k 03102022180354-testvid.mp4
 }
 
-
-
 "$@"
