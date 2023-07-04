7cce6dbaeab1882225a369d69bee4f5571c8a0f2
JoelNash-Xuuki <joelnash@xuuki.xyz>
2023-07-04 18:37:51 +0100

    modify main

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,22 @@
 #include "modsyn.hpp"
 
 int main(int argc, char **argv){
-  PrinterReader pr;
-  const char* patch= argv[1];
+  //PrinterReader pr;
+  //const char* patch= argv[1];
   const char* projName= argv[2];
-  ifstream readPatchFile(patch);
-  pr.readPatch();
+  //ifstream readPatchFile(patch);
+  //pr.readPatch();
 
-  //ProjectFileManager* pfm = new ProjectFileManager(projName);
+
+  //take me to main
+
+  ProjectFileManager* pfm = new ProjectFileManager(projName);
   AudioEngine * ae= new AudioEngine(44100,
         	  		    projName);
-  //ae->getControls(argc,argv);
+  ae->getControls(argc,argv);
+ 
  // cout << "Program ended." << endl;  
   return 0;
 }
+
+
