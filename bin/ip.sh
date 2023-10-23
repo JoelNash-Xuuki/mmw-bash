5613c05fdd247518c2b7df2296ddef084c43ddeb
JoelNash-Xuuki <joelnash@xuuki.xyz>
2023-10-23 13:31:49 +0100

    ip changes

--- bin/ip.sh
+++ bin/ip.sh
@@ -1,4 +1,4 @@
-source $HOME/mmw/tmp/options
+source $HOME/mmw/test/tmp/config
 
 addSub(){
   cp $MP4 $TEMP
@@ -7,46 +7,46 @@ addSub(){
 }
 
 			     addLyrics(){
-  ffmpeg -i $1\
-	  -vf "drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,2.619,3.52):textfile=$LYRICS1:x=(w-text_w)/2:y=(h-text_h)/2',
-	     drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,3.09,3.80):textfile=$LYRICS2:x=(w-text_w)/2:y=(h-text_h)/2',
-                          drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,3.334,4.09):textfile=$LYRICS3:x=(w-text_w)/2:y=(h-text_h)/2',
-                             drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,3.57,4.40):textfile=$LYRICS4:x=(w-text_w)/2:y=(h-text_h)/2',
-                             drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,4.00,4.86):textfile=$LYRICS5:x=(w-text_w)/2:y=(h-text_h)/2',
-                             drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,4.06,5.33):textfile=$LYRICS6:x=(w-text_w)/2:y=(h-text_h)/2',
-                             drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,5.68,6.00):textfile=$LYRICS7:x=(w-text_w)/2:y=(h-text_h)/2',
-                             drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,5.90,6.83):textfile=$LYRICS8:x=(w-text_w)/2:y=(h-text_h)/2',
-                             drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,7.17,7.50):textfile=$LYRICS9:x=(w-text_w)/2:y=(h-text_h)/2',
-                             drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,7.50,7.80):textfile=$LYRICS10:x=(w-text_w)/2:y=(h-text_h)/2',
-                             drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,7.80,8.20):textfile=$LYRICS11:x=(w-text_w)/2:y=(h-text_h)/2',
-			     drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,8.49,8.70):textfile=$LYRICS12:x=(w-text_w)/2:y=(h-text_h)/2',
-                             drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,8.70,09.06):textfile=$LYRICS13:x=(w-text_w)/2:y=(h-text_h)/2',
-                             drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,9.30,9.60):textfile=$LYRICS14:x=(w-text_w)/2:y=(h-text_h)/2',
-                             drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,9.60,9.75):textfile=$LYRICS15:x=(w-text_w)/2:y=(h-text_h)/2',
-                             drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,9.75,10.15):textfile=$LYRICS16:x=(w-text_w)/2:y=(h-text_h)/2',
-                             drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,10.15,10.85):textfile=$LYRICS17:x=(w-text_w)/2:y=(h-text_h)/2',
-                             drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,11.41,11.61):textfile=$LYRICS18:x=(w-text_w)/2:y=(h-text_h)/2',
-                             drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,11.61,12.00):textfile=$LYRICS19:x=(w-text_w)/2:y=(h-text_h)/2',
-                             drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,12.00,12.40):textfile=$LYRICS20:x=(w-text_w)/2:y=(h-text_h)/2',
-                             drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,12.40,12.70):textfile=$LYRICS21:x=(w-text_w)/2:y=(h-text_h)/2',
-                             drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,12.70,12.95):textfile=$LYRICS22:x=(w-text_w)/2:y=(h-text_h)/2',
-                             drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,13.10,13.43):textfile=$LYRICS23:x=(w-text_w)/2:y=(h-text_h)/2',
-                             drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,13.35,14.00):textfile=$LYRICS24:x=(w-text_w)/2:y=(h-text_h)/2',
-                             drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,14.05,14.45):textfile=$LYRICS25:x=(w-text_w)/2:y=(h-text_h)/2',
-                             drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,14.45,14.70):textfile=$LYRICS26:x=(w-text_w)/2:y=(h-text_h)/2',
-                             drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,15.00,15.26):textfile=$LYRICS27:x=(w-text_w)/2:y=(h-text_h)/2',
-                             drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,15.26,15.40):textfile=$LYRICS28:x=(w-text_w)/2:y=(h-text_h)/2',
-			     drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,15.70,15.80):textfile=$LYRICS29:x=(w-text_w)/2:y=(h-text_h)/2',
-                             drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,15.85,16.63):textfile=$LYRICS30:x=(w-text_w)/2:y=(h-text_h)/2',
-                             drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,16.68,16.88):textfile=$LYRICS31:x=(w-text_w)/2:y=(h-text_h)/2',
-                             drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,17.15,17.40):textfile=$LYRICS32:x=(w-text_w)/2:y=(h-text_h)/2',
-                             drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,17.50,17.62):textfile=$LYRICS33:x=(w-text_w)/2:y=(h-text_h)/2',
-                             drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,17.80,18.10):textfile=$LYRICS34:x=(w-text_w)/2:y=(h-text_h)/2',
-                             drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,18.15,18.60):textfile=$LYRICS35:x=(w-text_w)/2:y=(h-text_h)/2',
-                             drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,18.50,18.90):textfile=$LYRICS36:x=(w-text_w)/2:y=(h-text_h)/2',
-			     drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,18.95,19.05):textfile=$LYRICS37:x=(w-text_w)/2:y=(h-text_h)/2',
-			     drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,19.22,19.75):textfile=$LYRICS38:x=(w-text_w)/2:y=(h-text_h)/2' " $2;
-			     mv $2 $1
+ffmpeg -i $1\
+  -vf "drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,2.619,3.52):textfile=$LYRICS1:x=(w-text_w)/2:y=(h-text_h)/2',
+  drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,3.09,3.80):textfile=$LYRICS2:x=(w-text_w)/2:y=(h-text_h)/2',
+  drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,3.334,4.09):textfile=$LYRICS3:x=(w-text_w)/2:y=(h-text_h)/2',
+  drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,3.57,4.40):textfile=$LYRICS4:x=(w-text_w)/2:y=(h-text_h)/2',
+  drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,4.00,4.86):textfile=$LYRICS5:x=(w-text_w)/2:y=(h-text_h)/2',
+  drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,4.06,5.33):textfile=$LYRICS6:x=(w-text_w)/2:y=(h-text_h)/2',
+  drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,5.68,6.00):textfile=$LYRICS7:x=(w-text_w)/2:y=(h-text_h)/2',
+  drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,5.90,6.83):textfile=$LYRICS8:x=(w-text_w)/2:y=(h-text_h)/2',
+  drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,7.17,7.50):textfile=$LYRICS9:x=(w-text_w)/2:y=(h-text_h)/2',
+  drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,7.50,7.80):textfile=$LYRICS10:x=(w-text_w)/2:y=(h-text_h)/2',
+  drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,7.80,8.20):textfile=$LYRICS11:x=(w-text_w)/2:y=(h-text_h)/2',
+  drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,8.49,8.70):textfile=$LYRICS12:x=(w-text_w)/2:y=(h-text_h)/2',
+  drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,8.70,09.06):textfile=$LYRICS13:x=(w-text_w)/2:y=(h-text_h)/2',
+  drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,9.30,9.60):textfile=$LYRICS14:x=(w-text_w)/2:y=(h-text_h)/2',
+  drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,9.60,9.75):textfile=$LYRICS15:x=(w-text_w)/2:y=(h-text_h)/2',
+  drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,9.75,10.15):textfile=$LYRICS16:x=(w-text_w)/2:y=(h-text_h)/2',
+  drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,10.15,10.85):textfile=$LYRICS17:x=(w-text_w)/2:y=(h-text_h)/2',
+  drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,11.41,11.61):textfile=$LYRICS18:x=(w-text_w)/2:y=(h-text_h)/2',
+  drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,11.61,12.00):textfile=$LYRICS19:x=(w-text_w)/2:y=(h-text_h)/2',
+  drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,12.00,12.40):textfile=$LYRICS20:x=(w-text_w)/2:y=(h-text_h)/2',
+  drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,12.40,12.70):textfile=$LYRICS21:x=(w-text_w)/2:y=(h-text_h)/2',
+  drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,12.70,12.95):textfile=$LYRICS22:x=(w-text_w)/2:y=(h-text_h)/2',
+  drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,13.10,13.43):textfile=$LYRICS23:x=(w-text_w)/2:y=(h-text_h)/2',
+  drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,13.35,14.00):textfile=$LYRICS24:x=(w-text_w)/2:y=(h-text_h)/2',
+  drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,14.05,14.45):textfile=$LYRICS25:x=(w-text_w)/2:y=(h-text_h)/2',
+  drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,14.45,14.70):textfile=$LYRICS26:x=(w-text_w)/2:y=(h-text_h)/2',
+  drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,15.00,15.26):textfile=$LYRICS27:x=(w-text_w)/2:y=(h-text_h)/2',
+  drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,15.26,15.40):textfile=$LYRICS28:x=(w-text_w)/2:y=(h-text_h)/2',
+  drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,15.70,15.80):textfile=$LYRICS29:x=(w-text_w)/2:y=(h-text_h)/2',
+  drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,15.85,16.63):textfile=$LYRICS30:x=(w-text_w)/2:y=(h-text_h)/2',
+  drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,16.68,16.88):textfile=$LYRICS31:x=(w-text_w)/2:y=(h-text_h)/2',
+  drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,17.15,17.40):textfile=$LYRICS32:x=(w-text_w)/2:y=(h-text_h)/2',
+  drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,17.50,17.62):textfile=$LYRICS33:x=(w-text_w)/2:y=(h-text_h)/2',
+  drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,17.80,18.10):textfile=$LYRICS34:x=(w-text_w)/2:y=(h-text_h)/2',
+  drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,18.15,18.60):textfile=$LYRICS35:x=(w-text_w)/2:y=(h-text_h)/2',
+  drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,18.50,18.90):textfile=$LYRICS36:x=(w-text_w)/2:y=(h-text_h)/2',
+  drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,18.95,19.05):textfile=$LYRICS37:x=(w-text_w)/2:y=(h-text_h)/2',
+  drawtext=fontcolor=pink:fontsize=20:x=0:y=0:enable='between(t,19.22,19.75):textfile=$LYRICS38:x=(w-text_w)/2:y=(h-text_h)/2' " $2;
+  mv $2 $1
 }
 
 #joverlayWatermark(){
