c9eb8d84627bd2ef169e934f16e4cbd645fa0f0d
Joel Nash <joelnash@xuuki.xyz>
2023-10-27 02:22:26 +0100

    Refactor synmod.c to read input from a file instead of stdin

--- src/synmod.c
+++ src/synmod.c
@@ -3,33 +3,41 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char **argv){
-  OSCMOD *oscs;
-  int osc_count= 0;
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    printf("Usage: %s <filename>\n", argv[0]);
+    return 1;
+  }
+
+  FILE *file = fopen(argv[1], "r");
+  if (file == NULL) {
+    printf("Failed to open file: %s\n", argv[1]);
+    return 1;
+  }
+
+  OSCMOD *oscs = (OSCMOD *)malloc(MAXMODS * sizeof(OSCMOD));
+  int osc_count = 0;
   char modname[64];
 
-  oscs= (OSCMOD *) malloc(MAXMODS * sizeof(OSCMOD));
-
-  while(scanf("%s", modname) != EOF){
-    if(! strcmp(modname, "OSC")){
-      //Read in the data
-      scanf("%s", oscs[osc_count].sig_out);
-      scanf("%s", oscs[osc_count].frequency);
-      scanf("%s", oscs[osc_count].waveform);
-      scanf("%s", oscs[osc_count].sig_am);
-      scanf("%s", oscs[osc_count].sig_fm);
-      scanf("%s", oscs[osc_count].omin);
-      scanf("%s", oscs[osc_count].omax);
-      // Print it to make sure its ok
+  while (fscanf(file, "%s", modname) != EOF) {
+    if (!strcmp(modname, "OSC")) {
+      // Read in the data
+      fscanf(file, "%s", oscs[osc_count].sig_out);
+      fscanf(file, "%s", oscs[osc_count].frequency);
+      fscanf(file, "%s", oscs[osc_count].waveform);
+      fscanf(file, "%s", oscs[osc_count].sig_am);
+      fscanf(file, "%s", oscs[osc_count].sig_fm);
+      fscanf(file, "%s", oscs[osc_count].omin);
+      fscanf(file, "%s", oscs[osc_count].omax);
+      // Print it to make sure it's ok
       printf("%s %s %s %s %s %s %s\n",
-        oscs[osc_count].sig_out,
-        oscs[osc_count].frequency,
-        oscs[osc_count].waveform,
-        oscs[osc_count].sig_am,
-        oscs[osc_count].sig_fm,
-        oscs[osc_count].omin,
-        oscs[osc_count].omax
-      );
+             oscs[osc_count].sig_out,
+             oscs[osc_count].frequency,
+             oscs[osc_count].waveform,
+             oscs[osc_count].sig_am,
+             oscs[osc_count].sig_fm,
+             oscs[osc_count].omin,
+             oscs[osc_count].omax);
       ++osc_count;
     }
   }
