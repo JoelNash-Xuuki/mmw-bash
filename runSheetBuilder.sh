081ad43291dfb5d7bd0a61a8ff533b50db86962c
JoelNash-Xuuki <joelnash@xuuki.xyz>
2023-12-07 15:16:45 +0000

    Added *.swp to .gitignore Deleted Test-Sheet.ly Deleted runSheetBuilder.sh Deleted runSheetBuilderTest-2.sh Deleted test.patch

--- runSheetBuilder.sh
+++
@@ -1,4 +0,0 @@
-source $HOME/mmw/test/tmp/config
-mmw setProj Hello
-mmw createProj
-mmw buildSheetMusic $PROJPATH/src/score/$PROJ.ly Xuuki.xyz Hello Vyvn scorePatch $SCORE
--- runSheetBuilderTest-2.sh
+++
@@ -1,4 +0,0 @@
-source $HOME/mmw/test/tmp/config
-#mmw setProj Hello
-#mmw createProj
-mmw buildSheetMusic Test_Sheet.ly Xuuki Title Vyvn patchName  "/home/joel/mmw/test/src/"
