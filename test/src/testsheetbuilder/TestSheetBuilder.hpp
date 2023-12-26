c68682683ea80cef38d7b906d5ac45dd98b01ef6
JoelNash-Xuuki <joelnash@xuuki.xyz>
2023-12-26 15:08:51 +0000

    Updated test/src/testsheetbuilder/TestSheetBuilder.hpp to include the functions canReadPatchFileStaffGroupsStaffCountAndNoteCount and canReadPatchFileOfMultipleStaffGroupsStaffCountAndNoteCount.

--- test/src/testsheetbuilder/TestSheetBuilder.hpp
+++ test/src/testsheetbuilder/TestSheetBuilder.hpp
@@ -22,8 +22,8 @@ class TestSheetBuilder : public CppUnit::TestFixture
 {
   CPPUNIT_TEST_SUITE(TestSheetBuilder);
   CPPUNIT_TEST(canPrintScoreHeader);
-  //CPPUNIT_TEST(canReadPatchFileStaffGroupsStaffCountAndNoteCount);
-  //CPPUNIT_TEST(canReadPatchFileOfMultipleStaffGroupsStaffCountAndNoteCount);
+  CPPUNIT_TEST(canReadPatchFileStaffGroupsStaffCountAndNoteCount);
+  CPPUNIT_TEST(canReadPatchFileOfMultipleStaffGroupsStaffCountAndNoteCount);
   //CPPUNIT_TEST(canPrintStaffGroupHeader);
   //CPPUNIT_TEST(canPrintMultipleStaffGroupHeaders);
   //CPPUNIT_TEST(canPrintStaffInGroupHeader);
