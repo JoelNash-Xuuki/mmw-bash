19c540bec2f4d8dbe9ab001434cf8636bee913e5
JoelNash-Xuuki <joelnash@xuuki.xyz>
2023-12-26 14:56:49 +0000

    Update test/src/testsheetbuilder/TestSheetBuilder.cpp
    - Updated file paths for comparison in the `canPrintMultipleStaffInGroupHeader` function to match the new file structure.
    - Commented out several test cases that are not currently being used.
    
    No further information is provided to generate a complete git commit message.

--- test/src/testsheetbuilder/TestSheetBuilder.hpp
+++ test/src/testsheetbuilder/TestSheetBuilder.hpp
@@ -22,12 +22,12 @@ class TestSheetBuilder : public CppUnit::TestFixture
 {
   CPPUNIT_TEST_SUITE(TestSheetBuilder);
   CPPUNIT_TEST(canPrintScoreHeader);
-  CPPUNIT_TEST(canReadPatchFileStaffGroupsStaffCountAndNoteCount);
-  CPPUNIT_TEST(canReadPatchFileOfMultipleStaffGroupsStaffCountAndNoteCount);
-  CPPUNIT_TEST(canPrintStaffGroupHeader);
-  CPPUNIT_TEST(canPrintMultipleStaffGroupHeaders);
-  CPPUNIT_TEST(canPrintStaffInGroupHeader);
-  CPPUNIT_TEST(canPrintMultipleStaffInGroupHeader);
+  //CPPUNIT_TEST(canReadPatchFileStaffGroupsStaffCountAndNoteCount);
+  //CPPUNIT_TEST(canReadPatchFileOfMultipleStaffGroupsStaffCountAndNoteCount);
+  //CPPUNIT_TEST(canPrintStaffGroupHeader);
+  //CPPUNIT_TEST(canPrintMultipleStaffGroupHeaders);
+  //CPPUNIT_TEST(canPrintStaffInGroupHeader);
+  //CPPUNIT_TEST(canPrintMultipleStaffInGroupHeader);
   //CPPUNIT_TEST(canPrintNotesOnStaff);
   //CPPUNIT_TEST(canPrintStaffInGroupCloseBracket);
   //CPPUNIT_TEST(canAppendScoreFileSections);
