0fc7347c9a3fc08318082f8735ee16b62f8eb846
JoelNash-Xuuki <joelnash@xuuki.xyz>
2024-03-21 21:27:25 +0000

    Update ip.sh script to improve parameter handling and file manipulation. Fix the addSub and overlayImage functions to correctly process input arguments and update the file paths.

--- bin/ip.sh
+++ bin/ip.sh
@@ -35,14 +35,13 @@ addSub() {
   # Font style similar to CLI (monospaced), white font color, black background
   style="FontName=Courier New,FontSize=16,PrimaryColour=&H00FFFFFF,BackColour=&H00000000,BorderStyle=4,Outline=0,Shadow=0,Alignment=2,MarginL=40,MarginR=40,MarginV=$1"
 
-  ffmpeg -i $MP4R -vf "subtitles=$HOME/mmw/tmp/subtitles.srt:force_style='$style'" $MP4RTEMP
-
-  mv $MP4RTEMP $MP4R
+  ffmpeg -i $2 -vf "subtitles=$4:force_style='$style'" $3
+  mv $3 $2
 }
 
 overlayImage(){
-  ffmpeg -i $MP4R -vf "movie=$1[layer];[layer]scale=350:-1[layer2];[in][layer2]overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2:enable='between(t,($2),($3))'[out]" $MP4RTEMP
-  mv $MP4RTEMP $MP4R
+  ffmpeg -i $1 -vf "movie=$2[layer];[layer]scale=350:-1[layer2];[in][layer2]overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2:enable='between(t,($3),($4))'[out]" $5
+  mv $5 $1
 }
 
 "$@"
