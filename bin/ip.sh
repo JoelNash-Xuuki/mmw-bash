d56ac3d0c7d47fdc6abfa89145033bc41458722c
JoelNash-Xuuki <joelnash@xuuki.xyz>
2024-05-07 01:20:58 +0100

    xuuki logo behaviour

--- bin/ip.sh
+++ bin/ip.sh
@@ -7,12 +7,14 @@ runMethod(){
 }
 
 createTextPNG(){
+  width=485
+  height=155
   convert -background transparent -fill lightblue \
-	      -gravity center\
-          -extent 1920x1080\
-          -font ~/.fonts/xuukitype1.ttf -pointsize 288 label:$1 \
+          -extent $widthx$height\
+          -font ~/.fonts/xuukitype1.ttf -pointsize 36 label:$1\
            $HOME/images/$2.png
 }
+
 
 createMP4(){
   ffmpeg -ss 00:00:00 -i $1 -t $2 -filter_complex "[0:a]showspectrum=s=854x480:mode=combined:slide=scroll:saturation=0.2:scale=log,format=yuv420p[v]" -map "[v]" -map 0:a -b:v 700k -b:a 360k $3
@@ -56,3 +58,5 @@ generate_noise_image(){
 }
 
 "$@"
+
+
