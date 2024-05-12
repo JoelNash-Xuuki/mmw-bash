bad0998f5b1d3a15c3905cccdaa0dfb880e752d5
JoelNash-Xuuki <joelnash@xuuki.xyz>
2024-05-12 11:59:20 +0100

    change to font format

--- bin/ip.sh
+++ bin/ip.sh
@@ -15,8 +15,6 @@ createTextPNG(){
           -font ~/.fonts/xuukitype1.ttf -pointsize 36 label:$1\
            $HOME/images/$2.png
 }
-
-
 createMP4(){
   ffmpeg -ss 00:00:00 -i $1 -t $2 -filter_complex "[0:a]showspectrum=s=854x480:mode=combined:slide=scroll:saturation=0.2:scale=log,format=yuv420p[v]" -map "[v]" -map 0:a -b:v 700k -b:a 360k $3
 }
